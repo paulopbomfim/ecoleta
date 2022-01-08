515879f580f5a2771f7c6a0695b7b6e8b81c9446
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-01-08 01:02:30 -0300

    Homepage structure and style concluided

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,32 @@
 // import { useHistory } from 'react-router-dom';
 
-import './styles.scss';
-
 import logoImg from "../../assets/logo.svg";
 import registerImg from "../../assets/register.svg";
+import returnImg from "../../assets/return.svg";
 
-export function Header({linkPath, text}) {
+import styles from './styles.module.scss';
+
+export function Header({linkPath, isHome}) {
   // const history = useHistory();
   /*onClick={ history.push(linkPath) }*/
   return (
-    <header className="header-container">
+    <header className={ styles.container }>
       <img src={ logoImg } alt="Ecoleta" />
-      <div>
-        <img src={ registerImg } alt="Ícone representando função de registrar" />
-        <h3>
-          {text}
-        </h3>
-      </div>
+      {
+        isHome
+          ? (
+            <div>
+              <img src={ registerImg } alt="Ícone representando função de registrar" />
+              <h3>Cadastre um ponto de coleta</h3>
+            </div>
+          )
+          : (
+            <div>
+              <img src={ returnImg } alt="Ícone representando função de voltar pro início" />
+              <h3>Voltar para página inicial</h3>
+            </div>
+          )
+      }
     </header>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
 import { Header } from "../../components/Header";
 
+import searchImg from "../../assets/search.svg";
+import peopleImg from "../../assets/people.svg";
+
+import styles from "./styles.module.scss";
+
 export function Home() {
   return (
-    <div>
-      <Header linkPath={"/"} text={"Cadastre um ponto de coleta"}/>
+    <div className={ styles.homepageContainer }>
+      <Header linkPath={"/"}  isHome={ true }/>
+      <main className ={ styles.mainContainer }>
+        <div>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+          <button>
+            <img src={ searchImg } alt="Ícone de pesquisar" />
+            <strong>Pesquisar pontos de coleta</strong>
+          </button>
+        </div>
+        <img className={ styles.illustration } src={ peopleImg } alt="Pessoas reciclando lixo" />
+      </main>
     </div>
   )
 }
