2f6a6089bc3a1e079dc38792ec8d04edd04fd292
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-01-18 00:24:41 -0300

    README e modal de seleção de cidades criados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Header } from "../../components/Header";
 
 import searchImg from "../../assets/search.svg";
@@ -5,7 +8,33 @@ import peopleImg from "../../assets/people.svg";
 
 import styles from "./styles.module.scss";
 
+Modal.setAppElement('#root');
+
 export function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const modalStyle = {
+    overlay: {
+      backgroundColor: 'rgba(14, 10, 20, 0.9)',
+    },
+    content: {
+      background: 'none',
+      border: 'none',
+      top: '34%',
+      left: '39%',
+      right: '39%',
+      bottom: '33%',
+    }
+  }
+
+  function handleOpenModal() {
+    setIsModalOpen(true)
+  };
+
+  function handleCloseModal() {
+    setIsModalOpen(false)
+  }
+
   return (
     <div className={ styles.homepageContainer }>
       <Header linkPath={"/"}  isHome={ true }/>
@@ -13,13 +42,25 @@ export function Home() {
         <div>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-          <button>
+          <button onClick={ () => handleOpenModal() }>
             <img src={ searchImg } alt="Ícone de pesquisar" />
             <strong>Pesquisar pontos de coleta</strong>
           </button>
         </div>
         <img className={ styles.illustration } src={ peopleImg } alt="Pessoas reciclando lixo" />
       </main>
+      <Modal
+        isOpen={ isModalOpen }
+        onRequestClose={ () => handleCloseModal() }
+        style={ modalStyle }
+      >
+        <div className={ styles.modalContainer }>
+          <h2>Pontos de coleta</h2>
+          <input type="text" placeholder="Digite a cidade"/>
+          <input type="text" placeholder="Digite o estado"/>
+          <button>Buscar</button>
+        </div>
+      </Modal>
     </div>
   )
-}
+};
